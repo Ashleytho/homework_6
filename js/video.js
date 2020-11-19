819dfb35c5966c349193aceeaebf70098686046b
Ashleytho <60194041+Ashleytho@users.noreply.github.com>
2020-11-18 22:11:40 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,23 +1,31 @@
-var video;
+var video = document.getElementById("myVideo");
+var playbackSpeed = 1.0;
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 });
 
 document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	document.getElementById("volume").innerHTML = video.volume;
 	console.log("Play Video");
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	video.pause();
+	console.log("Pause Video");
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	
+	playbackSpeed -= .1;
+	video.playbackRate = playbackSpeed;
+	console.log("Playback Speed: "+ playbackSpeed);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	
+	playbackSpeed += .1;
+	video.playbackRate = playbackSpeed;
+	console.log("Playback Speed: "+ playbackSpeed);
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
